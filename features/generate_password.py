a18bf4be5027301dabd44fe4196814ab4e50bf83
Mohamed Yasser <mohamedwebdev94@gmail.com>
2026-05-28 09:46:54 +0300

    feat: add generate password

---
+++ features/generate_password.py
@@ -0,0 +1,12 @@
+import secrets
+import string
+
+
+def generate_password(length: int) -> str:
+    combination = string.ascii_letters + string.digits + "_-*#$^"
+    password = "".join(secrets.choice(combination) for i in range(length))
+
+    return password
+
+
+generate_password(21)
