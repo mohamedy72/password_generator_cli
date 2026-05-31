1c30ca5d89209aa2042e1cf5e6d8cef0337f1f0f
Mohamed Yasser <mohamedwebdev94@gmail.com>
2026-05-31 19:23:31 +0300

    feat: add key derivation

---
+++ helpers/crypto.py
@@ -0,0 +1,31 @@
+import secrets
+from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
+
+
+def generate_salt():
+    """
+    Genrate a salt of 32 hex characters to be used in password hashing
+
+    RETURN:
+        salt_string: hex encoded
+    """
+    randome_bytes = secrets.token_bytes(16)
+    salt_string = randome_bytes.hex()
+
+    return salt_string
+
+
+def derive_key(password: str, salt: str) -> bytes:
+    kdf = Argon2id(
+        salt=bytes.fromhex(salt),
+        length=32,
+        iterations=1,
+        lanes=4,
+        memory_cost=64 * 1024,
+        ad=None,
+        secret=None,
+    )
+    # .encode() transfer str => Bytes
+    key = kdf.derive(password.encode())
+
+    return key
